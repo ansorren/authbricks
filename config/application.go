a7f560b006bb3fa00514d261c850c9946cff9df3
Antonio Sorrentino <ansorrenltd@gmail.com>
2024-06-19 23:18:12 +0100

    application/config: Define initial configuration

--- config/application.go
+++ config/application.go
@@ -6,4 +6,16 @@ type Application struct {
 	RedirectURIs  []string
 	ResponseTypes []string
 	GrantTypes    []string
+	// PKCERequired is true if the authorization endpoint requires PKCE, even for confidential clients.
+	// Public clients are always required to use PKCE.
+	PKCERequired bool
+	// When S256CodeChallengeMethodRequired is true, the authorization endpoint requires the use of the S256 code challenge method.
+	// This effectively disables the `plain` code challenge method.
+	S256CodeChallengeMethodRequired bool
+	// AllowedAuthenticationMethods is the list of allowed authentication methods.
+	AllowedAuthenticationMethods []string
+
+	// Scopes is the list of scopes that the application is allowed to request.
+	// By default, if empty, the application is not allowed to request any scopes.
+	Scopes []string
 }
